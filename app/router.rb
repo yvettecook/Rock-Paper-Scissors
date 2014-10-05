83f7b62b7f704895a6801a4df6bcb53e132cafed
yvettecook <yvettecook@gmail.com>
2014-10-05 16:14:41 +0100

    Some refactoring and logic adding to results page

--- app/router.rb
+++ app/router.rb
@@ -37,20 +37,12 @@ class Play < Sinatra::Base
 
   post '/result' do
     @choice = params[:choice]
-    if session[:id] == 1 
-      PLAYER1.choice = params[:choice]
-    else
-      PLAYER2.choice = params[:choice]
-    end
+    session[:id] == 1 ? PLAYER1.choice = params[:choice] :PLAYER2.choice = params[:choice]
   	redirect '/result_page'
   end
 
   get '/result_page' do
-  	@player1_name = GAME.player1.name
-  	@player1_choice = GAME.player1.choice
-  	@player2_name = GAME.player2.name
-  	@player2_choice = GAME.player2.choice
-    @result = GAME.choices
+    
   	erb :result
   end
 
